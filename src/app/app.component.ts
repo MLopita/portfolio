eb98fb0ebacfac76c0827c81e7496ab683a36c49
lopita <lopita.mishra2001@gmail.com>
2025-04-11 16:22:17 +0530

    jjj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,7 +56,7 @@ export class AppComponent implements AfterViewInit {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
   downloadResume() {
-    const resumeUrl = "assets/pdf/Lopita_Mishra_Resume.pdf"; // Relative path
+    const resumeUrl = "pdf/Lopita_Mishra_Resume.pdf"; // Relative path
   
     // Open in a new tab
     const newTab = window.open(resumeUrl, "_blank");
@@ -92,6 +92,262 @@ export class AppComponent implements AfterViewInit {
 
 
   projects = [
+    //Project 1
+    {
+      title: 'Sales Insights Dashboard – ATILQ',
+      objective: 'To develop a real-time sales dashboard that enables ATILQ to monitor sales quantity, revenue, profit, and regional performance.',
+      summary: 'Built an automated Power BI dashboard connected to a centralized SQL Server database. Created interactive visuals to analyze revenue, profit margins, and customer segmentation. Implemented slicers for year/month filtering and dynamic region selection. Designed ETL pipeline to automate data flow, reducing manual effort by 20%. Supported better decision-making by surfacing key KPIs and underperforming areas.',
+      image: 'https://media.istockphoto.com/id/1488294044/photo/businessman-works-on-laptop-showing-business-analytics-dashboard-with-charts-metrics-and-kpi.jpg?s=1024x1024&w=is&k=20&c=VpSNiVam6Fw3egrJYnP28mEEAXyCjFRjqV_k4PK5S04=',
+      techStack: 'SQL Server, Power BI, ETL, Data Visualization, Automation',
+      link: 'https://drive.google.com/file/d/19h0Pe99JLymg_q6NaTU-nsSiHLK6Kiu1/view?usp=share_link',
+      visuals: [
+        {
+          title: 'Profit Analysis',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Profit%20Analysis.png?raw=true',
+          ],
+          points: [
+            'Revenue: ₹142.22M | Sales Quantity: 350K | Profit: ₹2.06M',
+            'City-wise profit and revenue visuals',
+            'Monthly trend (line chart) and customer performance'
+          ]
+        },
+        {
+          title: 'Performance Insight',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Performance%20Insights.png?raw=true',
+          ],
+          points: [
+            'Market-wise revenue contribution',
+            'Profit Target Slider',
+            'Bar & Line combo chart for region performance',
+            'Matrix showing customer-wise profit'
+          ]
+        },
+        {
+          title: 'Data Pane / Model ',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Data.png?raw=true',
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Model%20View.png?raw=true'
+          ],
+          points: [
+            'Show schema design, relationships, and calculated fields',
+            'Optional: show key DAX measures used for KPIs'
+          ]
+        }
+      ],
+      insights: [
+        'Excel and Electricalsara drove the highest profits.',
+        'Lucknow identified as an underperforming region (-2.7%).',
+        'Helped reduce sales spend by 10% through better regional strategy.',
+        'Enabled the team to dynamically filter and explore performance by city/product.'
+      ]
+    },
+    //Project 2
+    {
+      title: 'Sales Insights Dashboard – ATILQ',
+      objective: 'To develop a real-time sales dashboard that enables ATILQ to monitor sales quantity, revenue, profit, and regional performance.',
+      summary: 'Built an automated Power BI dashboard connected to a centralized SQL Server database. Created interactive visuals to analyze revenue, profit margins, and customer segmentation. Implemented slicers for year/month filtering and dynamic region selection. Designed ETL pipeline to automate data flow, reducing manual effort by 20%. Supported better decision-making by surfacing key KPIs and underperforming areas.',
+      image: 'https://media.istockphoto.com/id/1488294044/photo/businessman-works-on-laptop-showing-business-analytics-dashboard-with-charts-metrics-and-kpi.jpg?s=1024x1024&w=is&k=20&c=VpSNiVam6Fw3egrJYnP28mEEAXyCjFRjqV_k4PK5S04=',
+      techStack: 'SQL Server, Power BI, ETL, Data Visualization, Automation',
+      link: 'https://drive.google.com/file/d/19h0Pe99JLymg_q6NaTU-nsSiHLK6Kiu1/view?usp=share_link',
+      visuals: [
+        {
+          title: 'Profit Analysis',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Profit%20Analysis.png?raw=true',
+          ],
+          points: [
+            'Revenue: ₹142.22M | Sales Quantity: 350K | Profit: ₹2.06M',
+            'City-wise profit and revenue visuals',
+            'Monthly trend (line chart) and customer performance'
+          ]
+        },
+        {
+          title: 'Performance Insight',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Performance%20Insights.png?raw=true',
+          ],
+          points: [
+            'Market-wise revenue contribution',
+            'Profit Target Slider',
+            'Bar & Line combo chart for region performance',
+            'Matrix showing customer-wise profit'
+          ]
+        },
+        {
+          title: 'Data Pane / Model ',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Data.png?raw=true',
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Model%20View.png?raw=true'
+          ],
+          points: [
+            'Show schema design, relationships, and calculated fields',
+            'Optional: show key DAX measures used for KPIs'
+          ]
+        }
+      ],
+      insights: [
+        'Excel and Electricalsara drove the highest profits.',
+        'Lucknow identified as an underperforming region (-2.7%).',
+        'Helped reduce sales spend by 10% through better regional strategy.',
+        'Enabled the team to dynamically filter and explore performance by city/product.'
+      ]
+    },
+    //Project 3
+    {
+      title: 'Sales Insights Dashboard – ATILQ',
+      objective: 'To develop a real-time sales dashboard that enables ATILQ to monitor sales quantity, revenue, profit, and regional performance.',
+      summary: 'Built an automated Power BI dashboard connected to a centralized SQL Server database. Created interactive visuals to analyze revenue, profit margins, and customer segmentation. Implemented slicers for year/month filtering and dynamic region selection. Designed ETL pipeline to automate data flow, reducing manual effort by 20%. Supported better decision-making by surfacing key KPIs and underperforming areas.',
+      image: 'https://media.istockphoto.com/id/1488294044/photo/businessman-works-on-laptop-showing-business-analytics-dashboard-with-charts-metrics-and-kpi.jpg?s=1024x1024&w=is&k=20&c=VpSNiVam6Fw3egrJYnP28mEEAXyCjFRjqV_k4PK5S04=',
+      techStack: 'SQL Server, Power BI, ETL, Data Visualization, Automation',
+      link: 'https://drive.google.com/file/d/19h0Pe99JLymg_q6NaTU-nsSiHLK6Kiu1/view?usp=share_link',
+      visuals: [
+        {
+          title: 'Profit Analysis',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Profit%20Analysis.png?raw=true',
+          ],
+          points: [
+            'Revenue: ₹142.22M | Sales Quantity: 350K | Profit: ₹2.06M',
+            'City-wise profit and revenue visuals',
+            'Monthly trend (line chart) and customer performance'
+          ]
+        },
+        {
+          title: 'Performance Insight',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Performance%20Insights.png?raw=true',
+          ],
+          points: [
+            'Market-wise revenue contribution',
+            'Profit Target Slider',
+            'Bar & Line combo chart for region performance',
+            'Matrix showing customer-wise profit'
+          ]
+        },
+        {
+          title: 'Data Pane / Model ',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Data.png?raw=true',
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Model%20View.png?raw=true'
+          ],
+          points: [
+            'Show schema design, relationships, and calculated fields',
+            'Optional: show key DAX measures used for KPIs'
+          ]
+        }
+      ],
+      insights: [
+        'Excel and Electricalsara drove the highest profits.',
+        'Lucknow identified as an underperforming region (-2.7%).',
+        'Helped reduce sales spend by 10% through better regional strategy.',
+        'Enabled the team to dynamically filter and explore performance by city/product.'
+      ]
+    },
+    //Project 4
+    {
+      title: 'Sales Insights Dashboard – ATILQ',
+      objective: 'To develop a real-time sales dashboard that enables ATILQ to monitor sales quantity, revenue, profit, and regional performance.',
+      summary: 'Built an automated Power BI dashboard connected to a centralized SQL Server database. Created interactive visuals to analyze revenue, profit margins, and customer segmentation. Implemented slicers for year/month filtering and dynamic region selection. Designed ETL pipeline to automate data flow, reducing manual effort by 20%. Supported better decision-making by surfacing key KPIs and underperforming areas.',
+      image: 'https://media.istockphoto.com/id/1488294044/photo/businessman-works-on-laptop-showing-business-analytics-dashboard-with-charts-metrics-and-kpi.jpg?s=1024x1024&w=is&k=20&c=VpSNiVam6Fw3egrJYnP28mEEAXyCjFRjqV_k4PK5S04=',
+      techStack: 'SQL Server, Power BI, ETL, Data Visualization, Automation',
+      link: 'https://drive.google.com/file/d/19h0Pe99JLymg_q6NaTU-nsSiHLK6Kiu1/view?usp=share_link',
+      visuals: [
+        {
+          title: 'Profit Analysis',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Profit%20Analysis.png?raw=true',
+          ],
+          points: [
+            'Revenue: ₹142.22M | Sales Quantity: 350K | Profit: ₹2.06M',
+            'City-wise profit and revenue visuals',
+            'Monthly trend (line chart) and customer performance'
+          ]
+        },
+        {
+          title: 'Performance Insight',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Performance%20Insights.png?raw=true',
+          ],
+          points: [
+            'Market-wise revenue contribution',
+            'Profit Target Slider',
+            'Bar & Line combo chart for region performance',
+            'Matrix showing customer-wise profit'
+          ]
+        },
+        {
+          title: 'Data Pane / Model ',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Data.png?raw=true',
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Model%20View.png?raw=true'
+          ],
+          points: [
+            'Show schema design, relationships, and calculated fields',
+            'Optional: show key DAX measures used for KPIs'
+          ]
+        }
+      ],
+      insights: [
+        'Excel and Electricalsara drove the highest profits.',
+        'Lucknow identified as an underperforming region (-2.7%).',
+        'Helped reduce sales spend by 10% through better regional strategy.',
+        'Enabled the team to dynamically filter and explore performance by city/product.'
+      ]
+    },
+    //Project 5
+    {
+      title: 'Sales Insights Dashboard – ATILQ',
+      objective: 'To develop a real-time sales dashboard that enables ATILQ to monitor sales quantity, revenue, profit, and regional performance.',
+      summary: 'Built an automated Power BI dashboard connected to a centralized SQL Server database. Created interactive visuals to analyze revenue, profit margins, and customer segmentation. Implemented slicers for year/month filtering and dynamic region selection. Designed ETL pipeline to automate data flow, reducing manual effort by 20%. Supported better decision-making by surfacing key KPIs and underperforming areas.',
+      image: 'https://media.istockphoto.com/id/1488294044/photo/businessman-works-on-laptop-showing-business-analytics-dashboard-with-charts-metrics-and-kpi.jpg?s=1024x1024&w=is&k=20&c=VpSNiVam6Fw3egrJYnP28mEEAXyCjFRjqV_k4PK5S04=',
+      techStack: 'SQL Server, Power BI, ETL, Data Visualization, Automation',
+      link: 'https://drive.google.com/file/d/19h0Pe99JLymg_q6NaTU-nsSiHLK6Kiu1/view?usp=share_link',
+      visuals: [
+        {
+          title: 'Profit Analysis',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Profit%20Analysis.png?raw=true',
+          ],
+          points: [
+            'Revenue: ₹142.22M | Sales Quantity: 350K | Profit: ₹2.06M',
+            'City-wise profit and revenue visuals',
+            'Monthly trend (line chart) and customer performance'
+          ]
+        },
+        {
+          title: 'Performance Insight',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Performance%20Insights.png?raw=true',
+          ],
+          points: [
+            'Market-wise revenue contribution',
+            'Profit Target Slider',
+            'Bar & Line combo chart for region performance',
+            'Matrix showing customer-wise profit'
+          ]
+        },
+        {
+          title: 'Data Pane / Model ',
+          images: [
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Data.png?raw=true',
+            'https://github.com/MLopita/Sales-Insights-Dashboard-for-ATILQ/blob/main/Model%20View.png?raw=true'
+          ],
+          points: [
+            'Show schema design, relationships, and calculated fields',
+            'Optional: show key DAX measures used for KPIs'
+          ]
+        }
+      ],
+      insights: [
+        'Excel and Electricalsara drove the highest profits.',
+        'Lucknow identified as an underperforming region (-2.7%).',
+        'Helped reduce sales spend by 10% through better regional strategy.',
+        'Enabled the team to dynamically filter and explore performance by city/product.'
+      ]
+    },
+    //Project 6
     {
       title: 'Sales Insights Dashboard – ATILQ',
       objective: 'To develop a real-time sales dashboard that enables ATILQ to monitor sales quantity, revenue, profit, and regional performance.',
@@ -142,6 +398,7 @@ export class AppComponent implements AfterViewInit {
         'Enabled the team to dynamically filter and explore performance by city/product.'
       ]
     }
+    // add more projects
   ];
 
   selectedProject: any = null;
